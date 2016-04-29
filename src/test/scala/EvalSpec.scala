ffdef3730058161b088ee5046ecba55c5b10f292
tanishiking <rikiriki1238@gmail.com>
2016-04-29 19:36:36 +0900

    Add some tests for Eval and Environment

---
+++ src/test/scala/EvalSpec.scala
@@ -0,0 +1,18 @@
+import mlscala.Ast._
+import mlscala.Eval._
+import org.scalatest.{Matchers, FlatSpec}
+
+class EvalSpec extends FlatSpec with Matchers {
+
+  behavior of "Eval.scala"
+
+  it should "ILit(_)" in {
+    assert(applyPrim(Plus, ILit(1), ILit(3)) == ILit(4))
+    assert(applyPrim(Mult, ILit(3), ILit(3)) == ILit(9))
+  }
+
+  it should "BLit(_)" in {
+    assert(applyPrim(Lt, ILit(1), ILit(5)) == BLit(true))
+    assert(applyPrim(Lt, ILit(10), ILit(5)) == BLit(false))
+  }
+}
---
+++ src/test/scala/EvalWithEnvSpec.scala
@@ -0,0 +1,13 @@
+import mlscala.Ast._
+import mlscala.Eval._
+import mlscala.Environment._
+import org.scalatest.{Matchers, FlatSpec}
+
+class EvalWithEnvSpec extends FlatSpec with Matchers {
+
+  behavior of "Eval.scala and Environment.Scala"
+
+  it should "ILit(5)" in {
+    assert(evalExp(extendEnv(Var("x"), ILit(3), getEmptyEnv), BinOp(Plus, Var("x"), Var("x"))) == ILit(6))
+  }
+}
