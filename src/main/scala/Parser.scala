41b749d53d4841bfb917f34920b8da9783796041
tanishiking <rikiriki1238@gmail.com>
2016-04-29 19:57:41 +0900

    Wrap by Exp and fix parserspecs

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,34 +1,36 @@
 package mlscala
 
+import mlscala.Ast._
+
 import scala.util.parsing.combinator._
 
 object Parser extends RegexParsers {
 
   def parse(input: String) = parseAll(toplevel, input)
 
-  lazy val toplevel = expr <~ ";;"
+  lazy val toplevel = expr <~ ";;" ^^ { case e => Exp(e) }
 
   lazy val expr = ifExpr | ltExpr
 
   lazy val ltExpr = lessthan | pExpr
-  lazy val lessthan = pExpr ~ "<" ~ pExpr ^^ { case p1 ~ _ ~ p2 => Ast.BinOp(Ast.Lt, p1, p2)}
+  lazy val lessthan = pExpr ~ "<" ~ pExpr ^^ { case p1 ~ _ ~ p2 => BinOp(Lt, p1, p2)}
 
-  lazy val pExpr: Parser[Ast.Expr] = plus | mExpr
-  lazy val plus = mExpr ~ "+" ~ mExpr ^^ { case p1 ~ _ ~ p2 => Ast.BinOp(Ast.Plus, p1, p2) }
+  lazy val pExpr: Parser[Expr] = plus | mExpr
+  lazy val plus = mExpr ~ "+" ~ mExpr ^^ { case p1 ~ _ ~ p2 => BinOp(Plus, p1, p2) }
 
-  lazy val mExpr: Parser[Ast.Expr] = mult | aExpr
-  lazy val mult = aExpr ~ "*" ~ aExpr ^^ { case p1 ~ _ ~ p2 => Ast.BinOp(Ast.Mult, p1, p2) }
+  lazy val mExpr: Parser[Expr] = mult | aExpr
+  lazy val mult = aExpr ~ "*" ~ aExpr ^^ { case p1 ~ _ ~ p2 => BinOp(Mult, p1, p2) }
 
   lazy val aExpr = intv | truev | falsev | varv | paren
 
-  lazy val intv = """\d+""".r ^^ { case n => Ast.ILit(n.toInt) }
-  lazy val truev = "true" ^^ { case _ => Ast.BLit(true) }
-  lazy val falsev = "false" ^^ { case _ => Ast.BLit(false) }
-  lazy val varv = """[a-zA-Z]+""".r ^^ { case s => Ast.Var(s) }
+  lazy val intv = """\d+""".r ^^ { case n => ILit(n.toInt) }
+  lazy val truev = "true" ^^ { case _ => BLit(true) }
+  lazy val falsev = "false" ^^ { case _ => BLit(false) }
+  lazy val varv = """[a-zA-Z]+""".r ^^ { case s => Var(s) }
   lazy val paren = "(" ~> expr <~ ")"
 
-  lazy val ifExpr: Parser[Ast.Expr] = ("if" ~> expr <~ "then") ~ expr ~ ("else" ~> expr) ^^ {
-    case cond ~ et ~ ef => Ast.IfExp(cond, et, ef)
+  lazy val ifExpr: Parser[Expr] = ("if" ~> expr <~ "then") ~ expr ~ ("else" ~> expr) ^^ {
+    case cond ~ et ~ ef => IfExp(cond, et, ef)
   }
 
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -12,39 +12,39 @@ class ParserSpec extends FlatSpec with Matchers {
   }
 
   it should "Var(a)" in {
-    assert(parse("a;;").get == Var("a"))
-    assert(parse("test;;").get == Var("test"))
+    assert(parse("a;;").get == Exp(Var("a")))
+    assert(parse("test;;").get == Exp(Var("test")))
   }
 
   it should "BLit(true) or BLit(false)" in {
-    assert(parse("true;;").get == BLit(true))
-    assert(parse("false;;").get == BLit(false))
+    assert(parse("true;;").get == Exp(BLit(true)))
+    assert(parse("false;;").get == Exp(BLit(false)))
   }
 
   it should "ILit(num)" in {
-    assert(parse("1;;").get == ILit(1))
-    assert(parse("100;;").get == ILit(100))
+    assert(parse("1;;").get == Exp(ILit(1)))
+    assert(parse("100;;").get == Exp(ILit(100)))
   }
 
   it should "LessThan" in {
-    assert(parse("1 < 2;;").get == BinOp(Lt, ILit(1), ILit(2)))
-    assert(parse("1<2;;").get == BinOp(Lt, ILit(1), ILit(2)))
+    assert(parse("1 < 2;;").get == Exp(BinOp(Lt, ILit(1), ILit(2))))
+    assert(parse("1<2;;").get == Exp(BinOp(Lt, ILit(1), ILit(2))))
   }
 
   it should "plus" in {
-    assert(parse("1 + 2;;").get == BinOp(Plus, ILit(1), ILit(2)))
+    assert(parse("1 + 2;;").get == Exp(BinOp(Plus, ILit(1), ILit(2))))
   }
 
   it should "mult" in {
-    assert(parse("1 * 2;;").get == BinOp(Mult, ILit(1), ILit(2)))
+    assert(parse("1 * 2;;").get == Exp(BinOp(Mult, ILit(1), ILit(2))))
   }
 
   it should "nested binop" in {
-    assert(parse("(1 + 1) + 1;;").get == BinOp(Plus,BinOp(Plus,ILit(1),ILit(1)),ILit(1)))
+    assert(parse("(1 + 1) + 1;;").get == Exp(BinOp(Plus,BinOp(Plus,ILit(1),ILit(1)),ILit(1))))
   }
 
   it should "if" in {
-    assert(parse("if true then 1 else 0;;").get == IfExp(BLit(true),ILit(1),ILit(0)))
+    assert(parse("if true then 1 else 0;;").get == Exp(IfExp(BLit(true),ILit(1),ILit(0))))
   }
 
 }
